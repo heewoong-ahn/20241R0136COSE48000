8056bb72fbbf84fef14c442973d8d016d2b305f7
heewoong-ahn <dksgmldnd@korea.ac.kr>
2024-04-18 01:22:59 +0900

    feat: "connect to database"
    make a connection to psql using .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // 이 dir 을 root로 서버 DB를 수정해버려서 주의 필요!
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
